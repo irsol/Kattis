68b90f1ac45d1e6ece558af217729b83cd89d628
irsol <irinywka@gmail.com>
2020-03-06 17:06:15 +0100

    Added description

---
+++ spare_beds.py
@@ -0,0 +1,18 @@
+# k - spare beds for visiting kittens
+# Note that a kitten will only come if it can stay the whole time it wants to borrow a spare bed.
+
+# Input:
+# n and k (1≤𝑘<𝑛≤100000), the number of potential visiting kittens and the number of beds.
+# Then follow n lines, each containing two integers 𝑥𝑖 and 𝑦𝑖, meaning that kitten 𝑖 wants
+# to arrive at time 𝑥𝑖 and leave at time 𝑦𝑖.
+# This means that two kittens 𝑖 and 𝑗, where 𝑦𝑖=𝑥𝑗, can use the same bed, as one kitten leaves
+# at the same time as the other arrives. You may assume that 0≤𝑥𝑖<𝑦𝑖≤1000000000.
+
+# Output
+# Output the maximum number of kittens that can be housed given the requests.
+
+
+
+
+
+
